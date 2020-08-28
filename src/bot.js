78c5ad0798d15e218f80c640e5f558c3428ba5df
jose-Olvera <abraham.jolvera@gmail.com>
2020-08-27 20:55:20 -0700

    added rest of logic

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,21 @@
 require("dotenv").config();
-
 const { Client } = require('discord.js');
 const client = new Client();
 
 client.login(process.env.BOT_TOKEN);
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+ 
+ client.on('message', message => {
+    if (message.author.bot) return;
+    client.on('messageUpdate', (oldMessage, newMessage) => {
+        if(oldMessage.content === newMessage){
+            return;
+        } else {
+            message.channel.send(`YOO THIS DUDE JUST EDITED HIS MESSAGE LMAOOO 😂 😂 😂 😂`);
+        }
+     });  
+  });
